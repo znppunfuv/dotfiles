c4c054ac0afdba5c7f8e001482e288b529772f85
znppunfuv <znppunfuv@protonmail.com>
2020-07-28 17:31:12 +0900

    fix(zsh): zinit syntax

--- dotfiles/.config/zsh/plugin/zinit/zinit.zsh
+++ dotfiles/.config/zsh/plugin/zinit/zinit.zsh
@@ -18,7 +18,7 @@ autoload -Uz _zinit
 alias zinit-update='zinit update --all && zinit self-update && zinit compile --all'
 
 ### Plugins ###
-zinit ice wait='1' lucid \
+zinit ice wait=1 lucid \
     atclone='dircolors -b dircolors.256dark > color.zsh' \
     atpull='%atclone' \
     pick='color.zsh' \
@@ -26,7 +26,7 @@ zinit ice wait='1' lucid \
     atload='zstyle ":completion:*" list-colors "${(s.:.)LS_COLORS}"'
 zinit light seebi/dircolors-solarized
 
-zinit ice wait='0' lucid \
+zinit ice wait=0 lucid \
     atclone='
         . ./fast-syntax-highlighting.plugin.zsh
         fast-theme -q XDG:solarized-dark
@@ -34,7 +34,7 @@ zinit ice wait='0' lucid \
     atinit='zicompinit; zicdreplay'
 zinit light 'zdharma/fast-syntax-highlighting'
 
-zinit ice wait='0' lucid \
+zinit ice wait=0 lucid \
     pick='zsh-history-substring-search.zsh' \
     atload="
         bindkey '^[[A' history-substring-search-up
@@ -44,17 +44,17 @@ zinit ice wait='0' lucid \
     "
 zinit light zsh-users/zsh-history-substring-search
 
-zinit ice wait='0' lucid \
+zinit ice wait=0 lucid \
     atload="
         _zsh_autosuggest_start
         ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'
     "
 zinit light zsh-users/zsh-autosuggestions
 
-zinit ice wait='0' blockf lucid
+zinit ice wait=0 blockf lucid
 zinit light zsh-users/zsh-completions
 
-zinit ice wait='0' lucid
+zinit ice wait=0 lucid
 zinit light hlissner/zsh-autopair
 
 zinit ice \
@@ -85,7 +85,7 @@ install_powerline_precmd() {
 }
 install_powerline_precmd
 
-zinit ice wait='0' lucid \
+zinit ice wait=0 lucid \
     atload="
         bindkey '^G' anyframe-widget-cd-ghq-repository
         alias fcdr='anyframe-widget-cdr'
@@ -94,7 +94,7 @@ zinit ice wait='0' lucid \
     "
 zinit light mollifier/anyframe
 
-zinit ice wait='0' lucid \
+zinit ice wait=0 lucid \
     atinit='export FORGIT_GI_REPO_LOCAL="${XDG_DATA_HOME}/zsh/plugin/forgit"' \
     run-atpull atpull='forgit::ignore::update' \
     atload="
@@ -103,10 +103,10 @@ zinit ice wait='0' lucid \
     "
 zinit light wfxr/forgit
 
-zinit ice wait='0' lucid \
+zinit ice wait=0 lucid \
     atload="alias cdu='cd-gitroot'"
 zinit light mollifier/cd-gitroot
 
-zinit ice wait='0' lucid \
+zinit ice wait=0 lucid \
     atinit='. "$ZDOTDIR/.zshrc_lazy"'
 zinit light zdharma/null
